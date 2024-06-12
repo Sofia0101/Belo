cc3b1bfb9267638d1fd2762eeccac40c8176858b
Sofía Fornaroli <Sofia0101>
2024-06-12 03:54:09 -0300

    :truck: creo Head component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import Nav from "@/components/nav";
+import Head from "@/components/head";
 
 export default function Home() {
   return (
    <div className="">
     <div className="">
-    <section className="relative bg-transparent">
+    <section className="relative bg-[#6E00FF]">
     <Nav />
+    <Head />
+
+
+
+
+    
 
     </section>
     </div>
