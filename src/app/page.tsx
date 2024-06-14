961f978223036bf012d8bfc32ae0c1092e121ec3
Sofía Fornaroli <Sofia0101>
2024-06-14 01:01:56 -0300

    :alien: agrego boton y component steps.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Nav from "@/components/nav";
 import Head from "@/components/head";
 import Cards from "@/components/cards";
+import Steps from "@/components/steps";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <div className="container mx-auto max-w-screen-lg pt-20">
         <Head />
         <Cards />
+        <Steps />
+
       </div>
     </section>
   );
---
+++ src/components/steps.tsx
@@ -0,0 +1,11 @@
+export default function Steps() {
+  return (
+    <div className="flex justify-center items-center px-4 my-10">
+      <a className="w-full md:w-auto" href="">
+        <button className="w-[100%] md:w-[350px] rounded-xl border-[1px] border-opacity-[1] border-white bg-transparent pt-3 pb-3 text-sm leading-5 font-bold text-white transform-none cursor-pointer 
+        hover:bg-opacity-100 hover:bg-white hover:text-opacity-1 hover:text-black">¿Por qué belo?</button>
+      </a>
+    </div>
+
+  );
+}
